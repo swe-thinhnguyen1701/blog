47f286c029bf240f9df58826375f031dfbfdb4a7
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-07-04 20:53:50 -0500

    rename post by poster

---
+++ models/Poster.js
@@ -0,0 +1,39 @@
+const {Model, DataTypes} = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Poster extends Model {}
+
+Poster.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "user",
+                key: "id"
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        underscored: true,
+        freezeTableName: true,
+        model: "poster"
+    }   
+)
+
+module.exports = Poster;
