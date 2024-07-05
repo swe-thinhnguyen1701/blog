97a722c37820d0908ffa64a690b03ad39d24ac1c
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-07-04 21:03:18 -0500

    build connections of models.

---
+++ models/index.js
@@ -0,0 +1,28 @@
+const Poster = require("./Poster");
+const User = require("./User");
+const Comment = require("./Comment");
+
+User.hasMany(Poster, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE"
+});
+
+User.hasMany(Comment, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE"
+});
+
+Comment.belongsTo(User, {
+    foreignKey: "user_id"
+});
+
+Comment.belongsTo(Poster, {
+    foreignKey: "poster_id"
+});
+
+Poster.hasMany(Comment, {
+    foreignKey: "poster_id",
+    onDelete: "CASCADE"
+});
+
+module.exports = { Poster, Comment, User };
